ec4ece85809dff9dd98dba63e8f6b9d80b209a84
Jorge Ramirez <joramirez96@gmail.com>
2020-03-31 21:53:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,7 @@ def dennis_ritchies_language
         }
      }
      programmer_hash[:dennis_ritchie]
-     [:languages][0]
+     [:languages][1]
 end
 
 def adding_matz
@@ -82,7 +82,7 @@ def adding_matz
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => "C"
+          :languages => ["C"]
         }
      }
      programmer_hash[:yukihiro_matsumoto] = {
@@ -107,7 +107,7 @@ def changing_alan
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => "C"
+          :languages => ["C"]
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
